a4b37bf6ad15cdbc53e3a76c0385bf00ecfa4a91
Onno Feldmann <Neronno@NERONNO-XEON-X5>
2024-11-19 22:53:01 +0100

    êerst iidee föör 'n pluróólkörter

---
+++ WFDOTtoPDF/PluralShorter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFDOTtoPDF
+{
+    internal class PluralShorter
+    {
+        internal static string GetShortPlural(string word, string plural)
+        {
+            int substring = 0;
+            for (int i = 0; i < plural.Length; i++)
+            {
+                if (i >= word.Length || !plural[i].Equals(word[i]))
+                {
+                    substring = i;
+                    break;
+                }
+            }
+            if (substring < 3) return plural;
+            var shortplural = plural.Substring(substring, plural.Length - substring);
+            return $"-{shortplural}";
+        }
+    }
+}
